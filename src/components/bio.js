e0e0cf403f4b0ab7f451d34da1d9e5a9e2e87a17
Abel Zatarain <zatar002@cougars.csusm.edu>
2019-07-15 23:36:37 -0700

    initial commit

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,11 +54,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        <strong>{author}</strong> lives and works in San
+        Diego building useful things.
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
         </a>
       </p>
     </div>
